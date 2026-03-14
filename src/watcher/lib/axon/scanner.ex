4362172aa3663669ad429d1764c34675cdb4fbeb
Didier Stadelmann <didier.stadelmann@example.com>
2026-03-14 22:14:52 +0100

    feat(scanner): expose start_streaming to Elixir

--- src/watcher/lib/axon/scanner.ex
+++ src/watcher/lib/axon/scanner.ex
@@ -2,4 +2,5 @@ defmodule Axon.Scanner do
   use Rustler, otp_app: :axon_watcher, crate: "axon_scanner"
 
   def scan(_path), do: :erlang.nif_error(:nif_not_loaded)
+  def start_streaming(_path, _pid), do: :erlang.nif_error(:nif_not_loaded)
 end
