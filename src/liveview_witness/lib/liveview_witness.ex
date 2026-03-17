55d87284cfb27e9037a49e732e7916f2b803e081
Didier Stadelmann <didier.stadelmann@example.com>
2026-03-17 17:49:04 +0100

    feat: implement Elixir Contract API

---
+++ src/liveview_witness/lib/liveview_witness.ex
@@ -0,0 +1,27 @@
+defmodule LiveView.Witness do
+  @moduledoc """
+  Documentation for `LiveView.Witness`.
+  The Elixir Contract API for LiveView.Witness.
+  """
+
+  @doc """
+  Pushes a rendering contract to the client.
+
+  It generates a unique ID for the contract and pushes the "phx-witness:contract" event to the socket.
+
+  ## Examples
+
+      iex> LiveView.Witness.expect_ui(socket, ".project-grid")
+      {:ok, socket}
+
+  """
+  def expect_ui(socket, selector, expectations \\ []) do
+    contract = %{
+      id: :crypto.strong_rand_bytes(8) |> Base.encode16(),
+      selector: selector,
+      expectations: Map.new(expectations)
+    }
+
+    Phoenix.LiveView.push_event(socket, "phx-witness:contract", contract)
+  end
+end
