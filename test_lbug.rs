3917f918543e6e4aaa61753f9d8763d9f5775016
Didier Stadelmann <didier.stadelmann@example.com>
2026-04-06 15:13:24 +0200

    chore: consolidate cleanup of legacy test files

--- test_lbug.rs
+++
@@ -1,9 +0,0 @@
-use lbug::{Database, Connection, SystemConfig};
-fn main() {
-    let db = Database::new(":memory:", SystemConfig::default()).unwrap();
-    let conn = Connection::new(&db).unwrap();
-    let mut result = conn.query("RETURN 1").unwrap();
-    if let Some(row) = result.next() {
-        println!("Value: {}", row[0]);
-    }
-}
--- test_mix_load.exs
+++
@@ -1,4 +0,0 @@
-Mix.start()
-Code.require_file("/tmp/dummy_umbrella/apps/child_a/mix.exs")
-config = Mix.Project.config()
-IO.inspect(config)
